3a2f2caceac346ad6138072d9bdec3df21221fee
msleprosy <msleprosy@yandex.ru>
2019-03-25 01:14:49 +0300

    added xml in Solutions

--- src/com/epam/javacourse/storage/initor/datasourcereader/CountriesWithCitiesXmlDomParser.java
+++ src/com/epam/javacourse/storage/initor/datasourcereader/CountriesWithCitiesXmlDomParser.java
@@ -3,5 +3,83 @@ package com.epam.javacourse.storage.initor.datasourcereader;
 /**
  * Created by veronika on 25.03.2019.
  */
-public class CountriesWithCitiesXmlDomParser {
-}
+
+/*public class CountriesWithCitiesXmlDomParser implements FileParser<List<Country>> {
+
+   @Override
+    public List<Country> parseFile(String file) throws Exception {
+
+        Document doc = getDocument(file);
+        Element root = getOnlyElement(doc, "marks");
+
+        NodeList xmlMarks = root.getElementsByTagName("mark");
+        List<Mark> result = new ArrayList<>();
+
+        for (int i = 0; i < xmlMarks.getLength(); i++) {
+            result.add(getMarkFromXmlElement(xmlMarks.item(i)));
+        }
+        return result;
+    }
+
+    private Mark getMarkFromXmlElement(Node xmlMark) throws Exception {
+        Mark mark = new Mark();
+
+        mark.setName(getOnlyElementTextContent((Element) xmlMark, "name"));
+        mark.setCountry(getOnlyElementTextContent((Element) xmlMark, "country"));
+
+        NodeList models = ((Element) xmlMark).getElementsByTagName("model");
+        if (models.getLength() > 0) {
+            mark.setModels(new ArrayList<>());
+
+            for (int i = 0; i < models.getLength(); i++) {
+                Model model = getModelFromXmlElement((Element) models.item(i));
+                mark.getModels().add(model);
+            }
+        }
+        return mark;
+    }
+
+    private Model getModelFromXmlElement(Element modelXml) throws Exception {
+
+        String type = modelXml.getAttribute("type");
+        if (ModelDiscriminator.isDiscriminatorExists(type)) {
+            Model model = null;
+            switch (ModelDiscriminator.valueOf(type)) {
+
+                case PASSENGER: {
+                    model = new PassengerModel();
+                    PassengerModel passenger = (PassengerModel) model;
+                    passenger.setNumberOfSeats(parseInt(getOnlyElementTextContent(modelXml, "numberOfSeats")));
+                    passenger.setNumberOfAirbags(parseInt(getOnlyElementTextContent(modelXml, "numberOfAirbags")));
+                    passenger.setAudioSystemName(getOnlyElementTextContent(modelXml, "audioSystemName"));
+                    break;
+                }
+                case TRUCK: {
+                    model = new TruckModel();
+                    TruckModel truck = (TruckModel) model;
+                    truck.setTankSize(parseInt(getOnlyElementTextContent(modelXml, "tankSize")));
+                    truck.setEmbeddedKitchen("Y".equals(getOnlyElementTextContent(modelXml, "embeddedKitchen")));
+                    truck.setWeight(parseInt(getOnlyElementTextContent(modelXml, "weight")));
+                    break;
+                }
+            }
+
+            model.setName(getOnlyElementTextContent(modelXml, "name"));
+            model.setDescription(getOnlyElementTextContent(modelXml, "description"));
+
+            String stringValue = getOnlyElementTextContent(modelXml, "productionYearStart");
+            model.setProductionYearStart(parseInt(stringValue));
+
+            stringValue = getOnlyElementTextContentOrNull(modelXml, "productionYearEnd");
+            if (stringValue != null) {
+                model.setProductionYearEnd(parseInt(stringValue));
+            }
+
+            return model;
+        } else {
+            throw new InvalidModelDiscriminatorException(PARSE_MODEL_DISCRIMINATOR_ERROR.getCode(),
+                    PARSE_MODEL_DISCRIMINATOR_ERROR.getDescriptionAsFormatStr(type));
+        }
+    }
+
+}*/
--- src/com/epam/javacourse/storage/initor/datasourcereader/sax/CountriesWithCitiesXmlSaxParser.java
+++ src/com/epam/javacourse/storage/initor/datasourcereader/sax/CountriesWithCitiesXmlSaxParser.java
@@ -3,5 +3,16 @@ package com.epam.javacourse.storage.initor.datasourcereader.sax;
 /**
  * Created by veronika on 25.03.2019.
  */
-public class CountriesWithCitiesXmlSaxParser {
-}
+
+/*public class MarksWithModelsXmlSaxParser implements FileParser<List<Mark>> {
+
+
+    @Override
+    public List<Mark> parseFile(String file) throws Exception {
+        SAXParser saxParser = XmlSaxUtils.getParser();
+
+        MarksWithModelSaxHandler saxHandler = new MarksWithModelSaxHandler();
+        saxParser.parse(new File(file), saxHandler);
+        return saxHandler.getMarks();
+    }
+}*/
